b6ffca93b7773f64ccb80b4db8f2cc2639d6944e
Vasiliy Vasilyuk <xorcare@gmail.com>
2024-12-30 15:47:30 +0300

    Add uuid

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module dependabot-auto-merge
 
 go 1.23
 
-require github.com/xorcare/pointer v1.1.0
+require (
+	github.com/google/uuid v1.0.0
+	github.com/xorcare/pointer v1.1.0
+)
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/xorcare/pointer"
 )
 
 func main() {
-	fmt.Println("Hello", pointer.Bool(true))
+	fmt.Println("Hello", pointer.Bool(true), uuid.New())
 }
